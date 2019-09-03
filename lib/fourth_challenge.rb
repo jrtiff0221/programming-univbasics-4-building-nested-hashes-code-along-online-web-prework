2c0faea9fc01bfe1e86be64a6cae7b37b3cb92b3
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-03 17:17:28 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,7 +23,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-           :name => "Steven",
+        {
+          :name => "Steven",
           :age => "30",
           :attitude => "confused"
         }
