800836176837e89d64a4eb39904a9e63e4c951bd
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-03 17:04:48 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,21 +10,10 @@ def third_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"}
-      :heroine_friends => [
-        {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
-        }
-        {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
-      }
-    ]
-   }
-  }
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => []
+        
+       
 
   
 end
